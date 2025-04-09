955f618400f44dd19d2a7ee49f1a11e1271e3353
Shouvik <shouvik1999@gmail.com>
2025-04-09 00:52:57 -0500

    commit

---
+++ tester.py
@@ -0,0 +1,7 @@
+from utils.helpers import *
+from config.paths_config import *
+from pipeline.prediction_pipeline import hybrid_recommendation 
+
+#print(find_similar_animes('Fairy Tail', ANIME_WEIGHTS_PATH, ANIME2ANIME_ENCODED, ANIME2ANIME_DECODED, DF))
+
+print(hybrid_recommendation(11880))
